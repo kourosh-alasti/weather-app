6c021bdf9671e1d51483bb012a52e575c6cd0c42
Kourosh Alasti <coding@kouroshalasti.com>
2024-12-05 19:20:03 -0800

    Initial Commit

---
+++ WeatherStuff/WeatherStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherStuffApp.swift
+//  WeatherStuff
+//
+//  Created by Kourosh Alasti on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
