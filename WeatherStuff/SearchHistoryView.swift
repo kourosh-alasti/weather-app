f61dfd6c0f30650c1c59ed40f19f3af38475b887
Kourosh Alasti <coding@kouroshalasti.com>
2024-12-06 01:52:09 -0800

    feat: global search history
    see everyones search history here. deleting doesn't work haha

---
+++ WeatherStuff/SearchHistoryView.swift
@@ -0,0 +1,93 @@
+//
+//  SearchHistoryView.swift
+//  WeatherStuff
+//
+//  Created by Kourosh Alasti on 12/5/24.
+//
+
+import SwiftUI
+
+struct SearchHistoryView: View {
+    @State private var searchHistory: [CityInfo] = []
+    @State private var isLoading: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if isLoading {
+                    ProgressView("Loading Previous Searches...")
+                        .padding()
+                } else if searchHistory.isEmpty {
+                    Text("No Search History available.")
+                        .foregroundColor(.gray)
+                } else {
+                    
+                    List {
+                        ForEach(searchHistory, id: \.xata_id) { city in
+                            NavigationLink(destination: CityInfoView(city: city.city)){
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(city.city)
+                                            .font(.headline)
+                                        Text(String(format: "%.1f degC", city.lastTemperature))
+                                            .font(.subheadline)
+                                            .foregroundColor(.gray)
+                                    }
+                                    Spacer()
+                                }
+                            }
+                        }
+                        .onDelete(perform: deleteCity)
+                    }
+                }
+            }
+            .navigationTitle("Search History")
+            .onAppear {
+                fetchSearchHistory()
+            }
+        }
+    }
+    
+    private func fetchSearchHistory() {
+        guard let url = URL(string: "https://deploy-preview-1--unishopapp.netlify.app/api/") else {
+            print("Invalid URL")
+            return
+        }
+        
+        isLoading = true
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error fetching data: \(error.localizedDescription)")
+                isLoading = false
+                return
+            }
+            
+            guard let data = data else {
+                print("No Data Received")
+                isLoading = false
+                return
+            }
+            
+            do {
+                let decodedResponse = try JSONDecoder().decode(CityResponse.self, from: data)
+                DispatchQueue.main.async {
+                    searchHistory = decodedResponse.items
+                    isLoading = false
+                }
+            } catch {
+                print(data.count)
+                print("error decoding response: \(error.localizedDescription)")
+                isLoading = false
+            }
+        }.resume()
+    }
+    
+    private func deleteCity(at offsets: IndexSet) {
+        searchHistory.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    SearchHistoryView()
+}
